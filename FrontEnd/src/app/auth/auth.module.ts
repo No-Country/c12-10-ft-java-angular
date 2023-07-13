eaf476e147429613a7312865483c3e625698cd82
iarmolla <ivan.armolla@gmail.com>
2023-07-13 17:38:37 -0300

    create AuthRoutingModule for authentication

--- FrontEnd/src/app/auth/auth.module.ts
+++ FrontEnd/src/app/auth/auth.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent,
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
+
 export class AuthModule { }
