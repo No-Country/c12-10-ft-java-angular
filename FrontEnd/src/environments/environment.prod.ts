b859d96c0b1a618a47645d05c7b84f981b0c6014
Matias <matiasrdiaz@gmail.com>
2023-07-20 17:47:58 +0200

    changed dockerfile npm force and

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ const host = 'http://backend-service.default.svc.cluster.local:8082';
 
 export const environment = {
     production: false,
-    // url: host,
+    url: host,
     apiUrl: host,
 };
