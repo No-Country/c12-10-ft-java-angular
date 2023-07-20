34d6912da6154ef1a9d9e5261ab66b6c1d67ca23
Matias <matiasrdiaz@gmail.com>
2023-07-20 17:12:48 +0200

    changed env

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 const host = 'http://backend-service.default.svc.cluster.local:8082';
 
 export const environment = {
-    production: true,
+    production: false,
     // url: host,
     apiUrl: host,
 };
