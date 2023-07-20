b5bb1fe2b5cd34566d37840db8df6dca03f618e8
Matias <matiasrdiaz@gmail.com>
2023-07-20 16:00:57 +0200

    prod=true in environment.prod.ts

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 const host = 'http://backend-service.default.svc.cluster.local:8082';
 
 export const environment = {
-    production: false,
+    production: true,
     url: host,
     apiUrl: host,
 };
